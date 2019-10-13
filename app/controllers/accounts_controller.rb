7f9c260772cc7c0dd7023ad38858c8edbf35af04
Etienne van Delden-de la Haije <etienne@vandelden.family>
2019-10-13 20:25:07 +0200

    Remove some TODOs

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,8 +1,6 @@
 class AccountsController < ApplicationController
   before_action :set_account, only: [:show, :edit, :update, :destroy, :update_transactions]
 
-  # TODO: Allow an account to update all transactions that are associated to category_id
-
   # GET /accounts
   # GET /accounts.json
   def index
--- app/models/credit.rb
+++ app/models/credit.rb
@@ -1,3 +1,2 @@
 class Credit < Transaction
-  # TODO: Add a validator that the creditor_account is owned by us.
 end
