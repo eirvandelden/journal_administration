58005daaf1ecd5d963ef00cbb2ba4b48ab535afc
Etienne van Delden-de la Haije <etienne@vandelden.family>
2019-10-25 10:19:21 +0200

    Set transactions from & to our own accounts to be transfers

---
+++ db/data/20191024213804_set_transactions_from_etienne_or_michelle_to_samen_as_transfer.rb
@@ -0,0 +1,11 @@
+class SetTransactionsFromEtienneOrMichelleToSamenAsTransfer < ActiveRecord::Migration[6.0]
+  def up
+    our_account_ids = Account.where.not(owner: nil).ids
+    category_id = Category.find_by(name: 'Transfer').id
+    Transaction.where(debitor_account_id: our_account_ids, creditor_account_id: our_account_ids).where.not(type: 'Transfer').update_all(type: 'Transfer', category_id: category_id)
+  end
+
+  def down
+    raise ActiveRecord::IrreversibleMigration
+  end
+end
