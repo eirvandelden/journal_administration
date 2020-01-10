ea31366a6fa4e337e7bfc41555800092f44d8398
Etienne van Delden-de la Haije <etienne@vandelden.family>
2020-01-10 21:54:27 +0100

    :pencil: Add TODO: that need to be implemented

--- test/models/transaction_test.rb
+++ test/models/transaction_test.rb
@@ -1,34 +1,27 @@
 require 'test_helper'
 
 class TransactionTest < ActiveSupport::TestCase
+
+  # TODO: argument is an array of strings, not a string
   test 'A header string raises an error' do
     assert_raises Transaction::HeaderStringError do
       nil_transaction = Transaction.new '"Datum","Naam / Omschrijving","Rekening","Tegenrekening","Code","Af Bij","Bedrag (EUR)","MutatieSoort","Mededelingen"'
     end
   end
 
+  # TODO: van ons + naar ander + AF = credit
+  # TODO: van ons = naar ander + ander heeft een credit-category + af = transaction heeft dezesfde category
+  # TODO: van ons + naar ander + BIJ = debit
+  # TODO: van ons + naar ander + ander heeft een debit-category + bij = transaction heeft dezefde category
+  # TODO: van ons + naar ons = transfer
+  # TODO: van ons + naar ons = category "Transfer"
+  # TODO: beschrijving is nummer = onze rekening (potje)
+  # TODO: rekening is onbekend maakt een nieuw account
+  # TODO: tegenrekening is onbekend maakt een nieuw account
+
   # test 'It parsess values from a csv record' do
   #   parsed_transaction = Transaction.new '"20200229", "Etienne", "rekening", "tegenrekening", "GT", "Bij", "12,95", "Online bankieren", "Mededeling"'
 
   #   assert_equal Date.new(2020, 02, 29), parsed_transaction.booked_at
   # end
-  
-  # describe 'when given a header rule' do
-  #   test 'it returns a nil object'
-  # end
-
-  # describe 'with a debit transaction from others to us' do
-  # end
-
-  # describe 'with a credit transaction from us to others' do
-  # end
-
-  # describe 'with a transfer transaction from us, to us' do
-  # end
-
-  # describe 'with a valid transaction' do
-  #   describe 'with their account missing'
-  #   describe 'with our account missing'
-  # end
-
 end
