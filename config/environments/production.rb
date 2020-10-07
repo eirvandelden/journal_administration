234fea9330b1ef21ddd225cae55e648f0eec4556
Etienne van Delden-de la Haije <etienne@vandelden.family>
2020-10-07 12:02:11 +0200

    ✨ Allow production to respond to multiple hosts, via a ENV var

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  config.hosts.concat ENV["URL_HOSTS"]
 
   # Code is not reloaded between requests.
   config.cache_classes = true
