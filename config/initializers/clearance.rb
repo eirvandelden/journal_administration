34455e68774e6219e68e3670de278a61d37dc40b
Etienne van Delden-de la Haije <etienne@vandelden.family>
2020-12-08 23:28:27 +0100

    🔧 Move Clearance routing to router instead of implied, as per docs recommondations

--- config/initializers/clearance.rb
+++ config/initializers/clearance.rb
@@ -2,4 +2,5 @@ Clearance.configure do |config|
   config.mailer_sender          = "reply@example.com"
   config.rotate_csrf_on_sign_in = true
   config.allow_sign_up          = false
+  config.routes                 = false
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
+# rubocop:disable Metrics/BlockLength
 Rails.application.routes.draw do
   resources :category_groups
   resources :categories_groups
@@ -18,5 +19,18 @@ Rails.application.routes.draw do
   get "dashboard/index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  # Clearance routes
+  resources :passwords, controller: "clearance/passwords", only: %i[create new]
+  resource :session, controller: "clearance/sessions", only: [:create]
+
+  resources :users, controller: "clearance/users", only: [:create] do
+    resource :password, controller: "clearance/passwords", only: %i[create edit update]
+  end
+
+  get "/sign_in" => "clearance/sessions#new", as: "sign_in"
+  delete "/sign_out" => "clearance/sessions#destroy", as: "sign_out"
+  get "/sign_up" => "clearance/users#new", as: "sign_up"
+
   root "dashboard#index"
 end
+# rubocop:enable Metrics/BlockLength
