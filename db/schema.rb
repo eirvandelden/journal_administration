837e7004c206f6e6715aae125bfab04868e6b8ec
Etienne van Delden-de la Haije <etienne@vandelden.family>
2023-11-09 20:52:27 +0100

    🗃️Update db/schema due to change to sqlite

--- db/schema.rb
+++ db/schema.rb
@@ -2,76 +2,51 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# This file is the source Rails uses to define your schema when running `rails
-# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
 # be faster and is potentially less error prone than running all of your
 # migrations from scratch. Old migrations may fail to apply correctly if those
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_01_193351) do
-
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "btree_gin"
-  enable_extension "btree_gist"
-  enable_extension "citext"
-  enable_extension "cube"
-  enable_extension "dblink"
-  enable_extension "dict_int"
-  enable_extension "dict_xsyn"
-  enable_extension "earthdistance"
-  enable_extension "fuzzystrmatch"
-  enable_extension "hstore"
-  enable_extension "intarray"
-  enable_extension "ltree"
-  enable_extension "pg_stat_statements"
-  enable_extension "pg_trgm"
-  enable_extension "pgcrypto"
-  enable_extension "pgrowlocks"
-  enable_extension "pgstattuple"
-  enable_extension "plpgsql"
-  enable_extension "plv8"
-  enable_extension "tablefunc"
-  enable_extension "unaccent"
-  enable_extension "uuid-ossp"
-  enable_extension "xml2"
+ActiveRecord::Schema.define(version: 2023_11_09_194602) do
 
   create_table "accounts", force: :cascade do |t|
-    t.string "account_number"
-    t.string "name"
+    t.string "account_number", limit: 255
+    t.string "name", limit: 255
     t.integer "owner"
     t.bigint "category_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["category_id"], name: "index_accounts_on_category_id"
     t.index ["id"], name: "index_accounts_on_id"
   end
 
   create_table "categories", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.string "name", limit: 255
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.integer "direction"
-    t.bigint "parent_category_id"
+    t.integer "parent_category_id"
     t.index ["id"], name: "index_categories_on_id"
     t.index ["parent_category_id"], name: "index_categories_on_parent_category_id"
   end
 
-  create_table "data_migrations", primary_key: "version", id: :string, force: :cascade do |t|
+  create_table "data_migrations", primary_key: "version", id: { type: :string, limit: 255 }, force: :cascade do |t|
   end
 
   create_table "transactions", force: :cascade do |t|
     t.bigint "debitor_account_id"
     t.bigint "creditor_account_id"
-    t.decimal "amount", precision: 10, scale: 2
+    t.decimal "amount"
     t.datetime "booked_at"
     t.datetime "interest_at"
     t.bigint "category_id"
     t.text "note"
-    t.string "type"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.string "type", limit: 255
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.text "original_note"
     t.decimal "original_balance_after_mutation"
     t.string "original_tag"
@@ -80,9 +55,9 @@ ActiveRecord::Schema.define(version: 2020_09_01_193351) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.string "email", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "email", limit: 255, null: false
     t.string "encrypted_password", limit: 128, null: false
     t.string "confirmation_token", limit: 128
     t.string "remember_token", limit: 128, null: false
