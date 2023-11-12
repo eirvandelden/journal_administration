360dbc7c6e039f32e32ee248bb7081f2a2386a73
Etienne van Delden-de la Haije <etienne@vandelden.family>
2023-11-12 15:29:27 +0100

    🔧Set hosts where this app is hosted for dev/prod environments

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.hosts.concat ENV["HOSTS"].split(",")
+  config.hosts << "journal.test"
 
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
