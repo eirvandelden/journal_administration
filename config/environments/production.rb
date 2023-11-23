7c50524aa1a7c0d68f8c9546e3b57991708c4a4e
Etienne van Delden-de la Haije <etienne@vandelden.family>
2023-11-23 22:03:12 +0100

    🤐

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,6 +3,7 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   config.hosts << "finances.vandelden.family"
+  config.hosts << "192.168.1.154"
 
   # Code is not reloaded between requests.
   config.enable_reloading = false
