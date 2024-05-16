3ccec03589fdbebc81c77cb5fb62d299a79629c1
Etienne van Delden de la Haije <etienne@vandelden.family>
2024-05-16 22:10:17 +0200

    Update ruby version

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   config.hosts << "journal.vandelden.test"
+  config.hosts << "journal.test"
 
   # In the development environment your application's code is reloaded any time
   # it changes. This slows down response time but is perfect for development
