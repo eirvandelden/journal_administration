19317a05933390964a0a4bcde1b2bf0588d5a311
Etienne van Delden de la Haije <etienne@vandelden.family>
2025-09-11 22:30:27 +0200

    Set locale based on Current.user, fallback to english

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,13 @@ class ApplicationController < ActionController::Base
 
   # Disable origin-checking CSRF mitigation
   skip_before_action :verify_authenticity_token
+  before_action :locale
 
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  private
+    def locale
+      I18n.locale = Current.user&.locale || "en"
+    end
 end
