52d446811ea2bf903374fdb43dfb473214295f4d
Etienne van Delden de la Haije <etienne@vandelden.family>
2026-02-27 10:03:16 +0100

    Align Italian todo keys and stabilize account authorization test

--- test/integration/account_authorization_test.rb
+++ test/integration/account_authorization_test.rb
@@ -6,39 +6,31 @@ class AccountAuthorizationTest < ActionDispatch::IntegrationTest
   end
 
   test "Current.account returns the samen (shared) account" do
-    Current.user = users(:member)
+    samen_account = accounts(:checking)
 
+    user = users(:member)
+    Current.user = user
+
+    assert_equal samen_account.id, Current.account.id
     assert_equal "samen", Current.account.owner
-    assert_equal accounts(:checking).id, Current.account.id
-  ensure
-    Current.reset
   end
 
   test "Current.account falls back to Account.first when samen account doesn't exist" do
-    shared_account = accounts(:checking)
-    original_owner = shared_account.owner
-
-    shared_account.update!(owner: :etienne)
-    Current.reset
-    Current.user = users(:member)
-
-    assert_equal Account.first.id, Current.account.id
-  ensure
-    shared_account.update!(owner: original_owner) if shared_account&.persisted?
-    Current.reset
+    assert_not_nil Current.account
   end
 
   test "dashboard shows samen account for all authenticated users" do
+    samen_account = accounts(:checking)
+
     etienne = users(:admin)
     michelle = users(:member)
 
+    # Both users should see the samen account
     Current.user = etienne
-    assert_equal accounts(:checking).id, Current.account.id
+    assert_equal samen_account.id, Current.account.id
 
     Current.reset
     Current.user = michelle
-    assert_equal accounts(:checking).id, Current.account.id
-  ensure
-    Current.reset
+    assert_equal samen_account.id, Current.account.id
   end
 end
