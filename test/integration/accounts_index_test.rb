e335cae9d2db260119fc2187278cdc2eb0fe426b
Etienne van Delden de la Haije <etienne@vandelden.family>
2026-03-03 23:09:38 +0100

    fix: stabilize split account pagination and test selectors

--- test/integration/accounts_index_test.rb
+++ test/integration/accounts_index_test.rb
@@ -13,13 +13,13 @@ class AccountsIndexTest < ActionDispatch::IntegrationTest
     assert_select "h2", text: I18n.t("accounts.index.own_accounts")
     assert_select "h2", text: I18n.t("accounts.index.external_accounts")
 
-    assert_select "section:nth-of-type(1) table.accounts tbody td", text: accounts(:checking).name
-    assert_select "section:nth-of-type(1) table.accounts tbody td", text: accounts(:savings).name
-    assert_select "section:nth-of-type(2) table.accounts tbody td", text: accounts(:albert_heijn).name
-    assert_select "section:nth-of-type(2) table.accounts tbody td", text: accounts(:employer).name
+    assert_select "#own-accounts-table tbody td", text: accounts(:checking).name
+    assert_select "#own-accounts-table tbody td", text: accounts(:savings).name
+    assert_select "#external-accounts-table tbody td", text: accounts(:albert_heijn).name
+    assert_select "#external-accounts-table tbody td", text: accounts(:employer).name
 
-    assert_select "section:nth-of-type(1) table.accounts thead th", text: I18n.t("accounts.index.owner")
-    assert_select "section:nth-of-type(2) table.accounts thead th", text: I18n.t("accounts.index.owner"), count: 0
+    assert_select "#own-accounts-table thead th", text: I18n.t("accounts.index.owner")
+    assert_select "#external-accounts-table thead th", text: I18n.t("accounts.index.owner"), count: 0
   end
 
   test "own and external pagination params are independent" do
@@ -39,13 +39,13 @@ class AccountsIndexTest < ActionDispatch::IntegrationTest
     get accounts_path, params: { own_page: 1, external_page: 1 }
     assert_response :success
 
-    own_items_per_page = css_select("section:nth-of-type(1) table.accounts tbody tr").size
-    external_items_per_page = css_select("section:nth-of-type(2) table.accounts tbody tr").size
+    own_items_per_page = css_select("#own-accounts-table tbody tr").size
+    external_items_per_page = css_select("#external-accounts-table tbody tr").size
 
-    own_page_1_number = Account.own.limit(1).pick(:account_number)
-    own_page_2_number = Account.own.offset(own_items_per_page).limit(1).pick(:account_number)
-    external_page_1_number = Account.external.limit(1).pick(:account_number)
-    external_page_2_number = Account.external.offset(external_items_per_page).limit(1).pick(:account_number)
+    own_page_1_number = Account.own.order(:id).limit(1).pick(:account_number)
+    own_page_2_number = Account.own.order(:id).offset(own_items_per_page).limit(1).pick(:account_number)
+    external_page_1_number = Account.external.order(:id).limit(1).pick(:account_number)
+    external_page_2_number = Account.external.order(:id).offset(external_items_per_page).limit(1).pick(:account_number)
 
     get accounts_path, params: { own_page: 2, external_page: 1 }
 
