f26d5f3d7345180f4bfeb98fd481ed130f50f6a2
Etienne van Delden de la Haije <etienne@vandelden.family>
2026-03-04 15:49:02 +0100

    Add profile/admin navigation and remove session transfer flow

--- test/integration/main_navigation_test.rb
+++ test/integration/main_navigation_test.rb
@@ -3,6 +3,7 @@ require "test_helper"
 class MainNavigationTest < ActionDispatch::IntegrationTest
   setup do
     @member = users(:member)
+    @admin = users(:admin)
   end
 
   test "primary nav keeps home and journal as direct links" do
@@ -23,5 +24,25 @@ class MainNavigationTest < ActionDispatch::IntegrationTest
 
     assert_response :success
     assert_select "header nav a[href='#{transactions_path}']", text: I18n.t("common.back")
+  end
+
+  test "member sees profile link but not admin link" do
+    sign_in_as(@member)
+
+    get dashboard_index_url
+
+    assert_response :success
+    assert_select "header nav a[href='#{edit_user_profile_path(@member)}']", count: 1
+    assert_select "header nav a[href='#{admin_root_path}']", count: 0
+  end
+
+  test "administrator sees profile and admin links" do
+    sign_in_as(@admin)
+
+    get dashboard_index_url
+
+    assert_response :success
+    assert_select "header nav a[href='#{edit_user_profile_path(@admin)}']", count: 1
+    assert_select "header nav a[href='#{admin_root_path}']", count: 1
   end
 end
