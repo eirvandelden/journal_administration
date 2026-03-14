4c5d8d04e63702d45faedc477154d913b04ef417
Etienne van Delden de la Haije <etienne@vandelden.family>
2026-03-14 12:50:09 +0100

    Make Todo the only nav submenu

--- test/system/main_navigation_menus_test.rb
+++ test/system/main_navigation_menus_test.rb
@@ -17,31 +17,20 @@ class MainNavigationMenusTest < ApplicationSystemTestCase
     assert_todo_link(I18n.t("main_nav.todo_upload", locale: @locale), new_transactions_import_path)
   end
 
-  test "dashboard filter menu links keep the selected filter in the URL" do
-    assert_dashboard_filter_link(I18n.t("filters.month_to_date", locale: @locale), :month_to_date)
-    assert_dashboard_filter_link(I18n.t("filters.last_month", locale: @locale), :last_month)
-    assert_dashboard_filter_link(I18n.t("filters.three_months", locale: @locale), :three_months)
-    assert_dashboard_filter_link(I18n.t("filters.year_to_date", locale: @locale), :year_to_date)
-    assert_dashboard_filter_link(I18n.t("filters.last_year", locale: @locale), :last_year)
+  test "navigation does not show a separate filters menu" do
+    visit root_url
+
+    assert_no_selector "header nav summary", text: I18n.t("main_nav.dashboard_filters", locale: @locale)
   end
 
   private
     def assert_todo_link(label, path)
       visit root_url
-      open_nav_section(I18n.t("main_nav.todo_views", locale: @locale))
+      open_nav_section(I18n.t("main_nav.todo", model: Todo.model_name.human, locale: @locale))
 
       click_link label
 
       assert_current_path path, ignore_query: false
-    end
-
-    def assert_dashboard_filter_link(label, filter)
-      visit root_url
-      open_nav_section(I18n.t("main_nav.dashboard_filters", locale: @locale))
-
-      click_link label
-
-      assert_current_path dashboard_index_path(filter: filter), ignore_query: false
     end
 
     def open_nav_section(label)
