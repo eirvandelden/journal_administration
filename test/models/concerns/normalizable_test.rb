70e7ac71bb22db7740fcef52610f34f3a71d3ee8
Etienne van Delden de la Haije <etienne@vandelden.family>
2026-03-14 15:36:42 +0100

    Prefer longest account alias match

--- test/models/concerns/normalizable_test.rb
+++ test/models/concerns/normalizable_test.rb
@@ -7,6 +7,18 @@ class NormalizableTest < ActiveSupport::TestCase
     assert_equal accounts(:albert_heijn), account
   end
 
+  test "find_by_alias prefers the longest matching alias" do
+    shorter_match = Account.create!(name: "Lidl")
+    longer_match = Account.create!(name: "Lidl Online")
+
+    shorter_match.account_aliases.create!(pattern: "Lidl")
+    longer_match.account_aliases.create!(pattern: "Lidl Online")
+
+    account = Account.find_by_alias("Lidl Online GmbH")
+
+    assert_equal longer_match, account
+  end
+
   test "find_or_create_with_normalized_name creates new account when no alias matches" do
     account = Account.find_or_create_with_normalized_name("Some Unknown Store")
 
